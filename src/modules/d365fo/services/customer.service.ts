83259116b862bb678ca646d206a197e66c5eabae
Moataz Ali <moo3tazali@gmail.com>
2025-12-05 19:22:07 +0200

    style: fix indentation in customer service query builder

--- src/modules/d365fo/services/customer.service.ts
+++ src/modules/d365fo/services/customer.service.ts
@@ -15,7 +15,7 @@ export class CustomerService {
   constructor(
     private readonly d365foClient: D365FOClientService,
     private readonly queryBuilder: ODataQueryBuilderService,
-  ) { }
+  ) {}
 
   /**
    * Get customer list for a specific company
@@ -43,11 +43,11 @@ export class CustomerService {
     const baseFilter = this.queryBuilder.eq('dataAreaId', company);
     const filter = filters
       ? this.queryBuilder.and(
-        baseFilter,
-        Array.isArray(filters)
-          ? this.queryBuilder.buildFilterExpression(filters)
-          : filters,
-      )
+          baseFilter,
+          Array.isArray(filters)
+            ? this.queryBuilder.buildFilterExpression(filters)
+            : filters,
+        )
       : baseFilter;
 
     const query = this.queryBuilder.buildQuery('/data/Customers', {
