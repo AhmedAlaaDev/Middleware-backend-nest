fea45d4b12f7894067f477ec054c86ae1bcede81
Moataz Ali <moo3tazali@gmail.com>
2025-12-08 09:33:28 +0200

    refactor(accounts-receivable): simplify handler class and add required modules
    Remove unnecessary line breaks in handler class and use enum for processor type
    Add DataBatchModule, EntryProcessorsModule and ExcelModule imports

--- src/modules/accounts-receivable/accounts-receivable.module.ts
+++ src/modules/accounts-receivable/accounts-receivable.module.ts
@@ -4,6 +4,9 @@ import { CqrsModule } from '@nestjs/cqrs';
 import { AccountsReceivableController } from '@/modules/accounts-receivable/accounts-receivable.controller';
 import { ProcessARFreightHandler } from '@/modules/accounts-receivable/commands/handlers/process-ar-freight.handler';
 import { ProcessARTruckingHandler } from '@/modules/accounts-receivable/commands/handlers/process-ar-trucking.handler';
+import { DataBatchModule } from '@/modules/data-batch/data-batch.module';
+import { EntryProcessorsModule } from '@/modules/entry-processor/entry-processors.module';
+import { ExcelModule } from '@/modules/excel/excel.module';
 
 @Module({
   imports: [ExcelModule, EntryProcessorsModule, DataBatchModule, CqrsModule],
