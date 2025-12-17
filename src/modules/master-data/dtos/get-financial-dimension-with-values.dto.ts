8fc607461f96a20c2717a1ff271df626308bfbef
Moataz Ali <moo3tazali@gmail.com>
2025-12-17 16:12:46 +0200

    refactor(dto): update GetFinancialDimensionWithValuesDto to use JSDoc for property descriptions
    - Replaced ApiPropertyOptional decorators with JSDoc comments for financialKey and value properties, enhancing code readability and documentation clarity.

--- src/modules/master-data/dtos/get-financial-dimension-with-values.dto.ts
+++ src/modules/master-data/dtos/get-financial-dimension-with-values.dto.ts
@@ -1,20 +1,19 @@
-import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class GetFinancialDimensionWithValuesDto {
-  @ApiPropertyOptional({
-    description: 'Financial dimension key (e.g. Department, CostCenter, etc.)',
-    example: 'SubVendor',
-  })
+  /**
+   *
+   * Financial dimension key (e.g. Department, CostCenter, etc.)
+   * @example SubVendor
+   */
   @IsNotEmpty()
   @IsString()
   financialKey: string;
 
-  @ApiPropertyOptional({
-    description:
-      'Optional filter on dimension value (case-insensitive partial match)',
-    example: 'Su-000110',
-  })
+  /**
+   * Optional filter on dimension value (case-insensitive partial match)
+   * @example Su-000110
+   */
   @IsOptional()
   @IsString()
   value?: string;
